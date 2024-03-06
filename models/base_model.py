cdc6de6a1b3d6500774448bf4d286d0e6ef4ad76
Juman2312 <138710326+Juman2312@users.noreply.github.com>
2024-03-06 18:46:19 +0200

    j

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,18 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = kwargs.get('created_at', datetime.now())
-        self.updated_at = kwargs.get('updated_at', self.created_at)
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
         self.updated_at = datetime.now()
