a7cae5e9a03ebe1483634ecbe200c64c84886126
Juman2312 <138710326+Juman2312@users.noreply.github.com>
2024-03-09 20:11:27 +0200

    j

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,17 @@
-#!/usr/bin/python3
 import unittest
 from datetime import datetime
 import uuid
 from models.base_model import BaseModel
 
 class TestBaseModelInit(unittest.TestCase):
+    """Test cases for BaseModel initialization."""
+
     def setUp(self):
+        """Set up the test case."""
         self.base_model = BaseModel()
 
     def test_init(self):
+        """Test BaseModel initialization."""
         self.assertIsNotNone(self.base_model.id)
         self.assertIsInstance(self.base_model.created_at, datetime)
         self.assertIsInstance(self.base_model.updated_at, datetime)
@@ -16,10 +19,14 @@ class TestBaseModelInit(unittest.TestCase):
 
 
 class TestBaseModelSave(unittest.TestCase):
+    """Test cases for BaseModel save method."""
+
     def setUp(self):
+        """Set up the test case."""
         self.base_model = BaseModel()
 
     def test_save(self):
+        """Test BaseModel save method."""
         old_updated_at = self.base_model.updated_at
         self.base_model.save()
         new_updated_at = self.base_model.updated_at
@@ -27,10 +34,14 @@ class TestBaseModelSave(unittest.TestCase):
 
 
 class TestBaseModelToDict(unittest.TestCase):
+    """Test cases for BaseModel to_dict method."""
+
     def setUp(self):
+        """Set up the test case."""
         self.base_model = BaseModel()
 
     def test_to_dict(self):
+        """Test BaseModel to_dict method."""
         self.base_model.my_number = 123
         self.base_model.name = "Test Model"
         obj_dict = self.base_model.to_dict()
@@ -45,16 +56,23 @@ class TestBaseModelToDict(unittest.TestCase):
 
 
 class TestBaseModelStr(unittest.TestCase):
+    """Test cases for BaseModel __str__ method."""
+
     def setUp(self):
+        """Set up the test case."""
         self.base_model = BaseModel()
 
     def test_str(self):
+        """Test BaseModel __str__ method."""
         expected_string = f"[BaseModel] ({self.base_model.id}) {self.base_model.__dict__}"
         self.assertEqual(str(self.base_model), expected_string)
 
 
 class TestBaseModelInitFromDict(unittest.TestCase):
+    """Test cases for BaseModel initialization from dictionary."""
+
     def test_init_from_dict(self):
+        """Test BaseModel initialization from dictionary."""
         model_dict = {
             "id": "56d43177-cc5f-4d6c-a0c1-e167f8c27337",
             "created_at": "2017-09-28T21:03:54.052298",
