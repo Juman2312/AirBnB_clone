a7d3d57f1b5270a5c43ed103f1228cdb5a3eb452
Juman2312 <138710326+Juman2312@users.noreply.github.com>
2024-03-12 23:18:20 +0200

    l

--- models/base_model.py
+++ models/base_model.py
@@ -11,19 +11,24 @@ from models import storage
 class BaseModel:
     """Base class for all our classes"""
 
-    def __init__(self, my_number, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """Add a call to the method new(self) on storage"""
-        self.my_number = my_number
         if kwargs:
-            del kwargs['__class__']
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
-            super().__init__(*args, **kwargs)
+            self.__set_attributes(kwargs)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
+
+    def __set_attributes(self, attributes):
+        """Set the attributes"""
+        for key, value in attributes.items():
+            if key != "__class__":
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+
 
     def save(self):
         """Call save() method of storage"""
@@ -31,15 +36,8 @@ class BaseModel:
         storage.save()
 
     def to_dict(self):
-        obj_dict = {
-            'my_number': self.my_number,
-            'name': self.name,
-            '__class__': self.__class__.__name__,
-            'updated_at': self.updated_at.isoformat(),
-            'id': self.id,
-            'created_at': self.created_at.isoformat()
-        }
-        return obj_dict
-
-    def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        attributes = self.__dict__.copy()
+        attributes["__class__"] = self.__class__.__name__
+        attributes["created_at"] = attributes["created_at"].isoformat()
+        attributes["updated_at"] = attributes["updated_at"].isoformat()
+        return attributes
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,10 +13,8 @@ class FileStorage:
     """
         This is  will serve as an Object relation mappingto interface or database
     """
-
-    def __init__(self, file_path="C:\Users\Dedo-PC\Desktop\AirBnB_clone\models\engine\file_storage.py"):
-        self.file_path = file_path
-        self.__objects = {}
+    __file_path = "C:\Users\Dedo-PC\Desktop\AirBnB_clone\models\engine\file_storage.py"
+    __objects = {}
 
     def all(self):
         """Returns the dictionary __objects"""
@@ -24,30 +22,25 @@ class FileStorage:
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        key = obj.__class__.__name__ + '.' + obj.id
         self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file"""
-        json_dict = {}
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        serialized_objects = {}
         for key, obj in self.__objects.items():
-            json_dict[key] = obj.to_dict()
-        try:
-            with open(self.file_path, 'w') as file:
-                json.dump(json_dict, file)
-        except IOError:
-            print(f"Error: Unable to write to file {self.file_path}")
+            serialized_objects[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
         try:
-            with open(self.file_path, 'r') as file:
-                json_dict = json.load(file)
-                for key, value in json_dict.items():
+            with open(self.__file_path, 'r') as file:
+                serialized_objects = json.load(file)
+                for key, obj_dict in serialized_objects.items():
                     class_name, obj_id = key.split('.')
-                    class_ = eval(class_name)
-                    self.__objects[key] = class_(**value)
+                    obj = globals()[class_name](**obj_dict)
+                    self.__objects[key] = obj
         except FileNotFoundError:
-            print(f"Error: File {self.file_path} not found")
-        except (json.JSONDecodeError, ValueError):
-            print(f"Error: Invalid JSON in file {self.file_path}")
+            pass
